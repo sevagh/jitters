f5f4fc5a6dbaf2cc6185d3ea34132bfb8951ee4a
Sevag Hanssian <sevag.hanssian@gmail.com>
2019-08-13 20:02:04 -0400

    Begin basic UDP stream implementation

---
+++ src/lib.rs
@@ -0,0 +1,154 @@
+use byteorder::{ByteOrder, NetworkEndian};
+use rand::{thread_rng, Rng};
+//use hound::{WavReader, WavSpec};
+use std::net::UdpSocket;
+use std::{cmp::min, mem::size_of};
+
+const MAX_PACKET_SIZE: usize = 1400; //some voodoo based on 1500 mtu
+
+pub struct RtpOutStream {
+    flags: u16,
+    sequence: u16,
+    timestamp: u32,
+    ssrc: u32,
+}
+
+pub struct RtpUdpOutStream {
+    r: RtpOutStream,
+    socket: UdpSocket,
+}
+
+#[repr(C, align(1))]
+pub struct RtpHeader {
+    flags: u16,
+    sequence: u16,
+    timestamp: u32,
+    ssrc: u32,
+}
+
+impl RtpOutStream {
+    pub fn new(channels: u16) -> Self {
+        let sequence = thread_rng().gen::<u16>();
+        let timestamp = thread_rng().gen::<u32>();
+        let ssrc = thread_rng().gen::<u32>();
+
+        let pt: u16 = match channels {
+            1 => 0b1010,
+            2 => 0b1011,
+            _ => panic!(),
+        }; //L16, 44100, mono vs stereo
+
+        let flags: u16 = 0b000000010 | (pt << 9);
+        //                 MCCCCXPVV
+
+        return RtpOutStream {
+            flags,
+            sequence,
+            timestamp,
+            ssrc,
+        };
+    }
+
+    fn next_packet(&mut self, audio_slice: &[u8], timestamp_delta: u32) -> Vec<u8> {
+        let ret_size = min(MAX_PACKET_SIZE, size_of::<RtpHeader>() + audio_slice.len());
+        if ret_size > MAX_PACKET_SIZE {
+            panic!("fuck you")
+        }
+
+        let hdr = self.construct_header();
+
+        let mut ret = vec![0u8; ret_size];
+
+        NetworkEndian::write_u16(&mut ret, hdr.flags);
+        NetworkEndian::write_u16(&mut ret[2..], hdr.sequence);
+        NetworkEndian::write_u32(&mut ret[4..], hdr.timestamp);
+        NetworkEndian::write_u32(&mut ret[8..], hdr.ssrc);
+
+        ret[size_of::<RtpHeader>()..].copy_from_slice(audio_slice);
+
+        self.increment(timestamp_delta);
+        ret
+    }
+
+    fn increment(&mut self, timestamp_delta: u32) {
+        self.timestamp += timestamp_delta;
+        self.sequence += 1;
+    }
+
+    fn construct_header(&self) -> RtpHeader {
+        RtpHeader {
+            flags: self.flags,
+            ssrc: self.ssrc,
+            timestamp: self.timestamp,
+            sequence: self.sequence,
+        }
+    }
+}
+
+impl RtpUdpOutStream {
+    pub fn new(channels: u16, hostport: &str) -> Self {
+        RtpUdpOutStream {
+            r: RtpOutStream::new(channels),
+            socket: UdpSocket::bind(hostport).unwrap(),
+        }
+    }
+
+    pub fn send_next_packet(&mut self, audio_slice: &[u8], timestamp_delta: u32, dst: &str) {
+        let pkt = self.r.next_packet(audio_slice, timestamp_delta);
+        self.socket.send_to(&pkt, dst).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use memoffset::offset_of;
+    use std::mem::size_of;
+
+    #[test]
+    fn test_max_packet_size() {
+        // this looks like a stupid test but it should ensure that i don't blindly change
+        // MAX_PACKET_SIZE without considering the implications
+        assert_eq!(MAX_PACKET_SIZE, 1400);
+    }
+
+    #[test]
+    fn test_size_of() {
+        assert_eq!(size_of::<RtpHeader>(), 12);
+    }
+
+    #[test]
+    fn test_offset_of() {
+        assert_eq!(offset_of!(RtpHeader, flags), 0);
+        assert_eq!(offset_of!(RtpHeader, sequence), 2);
+        assert_eq!(offset_of!(RtpHeader, timestamp), 4);
+        assert_eq!(offset_of!(RtpHeader, ssrc), 8);
+    }
+
+    #[test]
+    fn test_out_stream_packets() {
+        // mono channel
+        let mut rtp_stream = RtpOutStream::new(1);
+
+        let test_data_1 = vec![1u8, 3u8, 5u8, 7u8];
+        let test_data_2 = vec![2u8, 4u8, 6u8, 8u8];
+
+        let packet_1 = rtp_stream.next_packet(&test_data_1, 1);
+        let packet_2 = rtp_stream.next_packet(&test_data_2, 2);
+
+        println!("packet 1: {:#?}", packet_1);
+        println!("packet 2: {:#?}", packet_2);
+    }
+
+    #[test]
+    fn test_out_udp() {
+        // mono channel
+        let mut rtp_stream = RtpUdpOutStream::new(1, "127.0.0.1:1337");
+
+        let test_data_1 = vec![1u8, 3u8, 5u8, 7u8];
+        let test_data_2 = vec![2u8, 4u8, 6u8, 8u8];
+
+        rtp_stream.send_next_packet(&test_data_1, 1, "127.0.0.1:1234");
+        rtp_stream.send_next_packet(&test_data_2, 2, "127.0.0.1:1234");
+    }
+}
